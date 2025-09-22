ce9e28023b7ac4a84ddae01d8bc1c9fde8931817
sipon <fakhrulislamsipon@gmail.com>
2025-09-22 15:39:58 +0600

    basic server side setup

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
   
+   const db = client.db('codeClash');
+   const problemCollection = db.collection('problem');
 
-    
 
 
 
